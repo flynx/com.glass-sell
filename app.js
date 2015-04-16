9fe2ecf2fd85ce939c8ff6318703133709e21a47
Alex A. Naanou <alex.nanou@gmail.com>
2015-04-16 15:00:50 +0300

    some cleanup and some work on persistent sessions...
    Signed-off-by: Alex A. Naanou <alex.nanou@gmail.com>

--- app.js
+++ app.js
@@ -1,21 +1,23 @@
-var express = require('express');
+var express = require('express')
 var forceSSL = require('express-force-ssl')
+//var mongodb = require('mongodb')
 var session = require('express-session')
-var path = require('path');
-var favicon = require('serve-favicon');
-var logger = require('morgan');
-var cookieParser = require('cookie-parser');
-var bodyParser = require('body-parser');
+//var MongoStore = require('connect-mongo')(session)
+var path = require('path')
+var favicon = require('serve-favicon')
+var logger = require('morgan')
+var cookieParser = require('cookie-parser')
+var bodyParser = require('body-parser')
 
-var sessionRoutes = require('./routes/session');
-var index = require('./routes/index');
-var users = require('./routes/users');
+var sessionRoutes = require('./routes/session')
+var index = require('./routes/index')
+var users = require('./routes/users')
 
-var app = express();
+var app = express()
 
 // view engine setup
-app.set('views', path.join(__dirname, 'views'));
-app.set('view engine', 'jade');
+app.set('views', path.join(__dirname, 'views'))
+app.set('view engine', 'jade')
 
 
 // passport...
@@ -33,26 +35,26 @@ passport.use(new LocalStrategy(
 		/*
 		User.findOne({ username: username }, function(err, user) {
 			if (err) { 
-	return done(err); 
+	return done(err) 
 			}
 			if (!user) {
-				return done(null, false, { message: 'Incorrect username.' });
+				return done(null, false, { message: 'Incorrect username.' })
 			}
 			if (!user.validPassword(password)) {
-				return done(null, false, { message: 'Incorrect password.' });
+				return done(null, false, { message: 'Incorrect password.' })
 			}
-			return done(null, user);
-		});
+			return done(null, user)
+		})
 		*/
 	}
-));
+))
 
 
 // XXX return session id/data to session...
 passport.serializeUser(function(user, done) {
 		console.log('serializeUser: ' + user)
-		done(null, user);
-});
+		done(null, user)
+})
 
 // XXX check if session is still valid...
 passport.deserializeUser(function(user, done) {
@@ -61,11 +63,11 @@ passport.deserializeUser(function(user, done) {
 /*
 		db.users.findById(id, function(err, user){
 				console.log(user)
-				if(!err) done(null, user);
+				if(!err) done(null, user)
 				else done(err, null)	
 		})
 		*/
-});
+})
 
 
 // auth middleware...
@@ -77,43 +79,50 @@ function authenticated(req, res, next){
 		//	...and open the path on login
 		//	XXX store the path in session to get back to it when we are done...
 		req.session.url = req.url
-		res.render('login', { title: 'Express' });
-		//res.redirect('/login');
+		res.render('login', { title: 'Express' })
+		//res.redirect('/login')
 	}
 }
 
 
 // uncomment after placing your favicon in /public
-//app.use(favicon(__dirname + '/public/favicon.ico'));
-app.use(logger('dev'));
-app.use(bodyParser.json());
-app.use(bodyParser.urlencoded({ extended: false }));
-app.use(forceSSL);
-app.use(cookieParser());
-app.use(express.static(path.join(__dirname, 'public')));
+//app.use(favicon(__dirname + '/public/favicon.ico'))
+app.use(logger('dev'))
+app.use(forceSSL)
+app.use(bodyParser.json())
+app.use(bodyParser.urlencoded({ extended: false }))
+app.use(cookieParser())
+app.use(express.static(path.join(__dirname, 'public')))
+// NOTE: this must be after the static stuff...
 app.use(session({
 	secret: 'keyboard cat',
 	resave: false,
 	saveUninitialized: false,
-}));
-app.use(passport.initialize());
-app.use(passport.session());
+	//		m	 s	  ms
+	maxAge: 20 * 60 * 1000,
+	// XXX uses MemoryStore, swithc to Mongo ASAP...
+	// 		see: https://www.npmjs.com/package/connect-mongo
+	//store: new MongoStore({
+	//}), 
+}))
+app.use(passport.initialize())
+app.use(passport.session())
 
 
-app.use('/', sessionRoutes);
-app.use('/', authenticated, index);
+app.use('/', sessionRoutes)
+app.use('/', authenticated, index)
 
 // XXX add roles...
-app.use('/users', authenticated, users);
+app.use('/users', authenticated, users)
 
 
 
 // catch 404 and forward to error handler
 app.use(function(req, res, next) {
-	var err = new Error('Not Found');
-	err.status = 404;
-	next(err);
-});
+	var err = new Error('Not Found')
+	err.status = 404
+	next(err)
+})
 
 // error handlers
 
@@ -121,25 +130,25 @@ app.use(function(req, res, next) {
 // will print stacktrace
 if (app.get('env') === 'development') {
 	app.use(function(err, req, res, next) {
-		res.status(err.status || 500);
+		res.status(err.status || 500)
 		res.render('error', {
 			message: err.message,
 			error: err
-		});
-	});
+		})
+	})
 }
 
 // production error handler
 // no stacktraces leaked to user
 app.use(function(err, req, res, next) {
-	res.status(err.status || 500);
+	res.status(err.status || 500)
 	res.render('error', {
 		message: err.message,
 		error: {}
-	});
-});
+	})
+})
 
 
-module.exports = app;
+module.exports = app
 
 // vim:set ts=4 sw=4 :
