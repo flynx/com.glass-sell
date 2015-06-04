8fdf32052198f4a2b33f250678c155436f58b8b5
Alex A. Naanou <alex.nanou@gmail.com>
2015-06-04 03:20:20 +0300

    added compatible ecode listing...
    Signed-off-by: Alex A. Naanou <alex.nanou@gmail.com>

--- models/car.js
+++ models/car.js
@@ -32,4 +32,65 @@ var Car =
 module.exports = mongoose.model('Car', CarSchema)
 
 
+// XXX this is preferable to the mapReduce version below...
+// XXX test...
+Car.getCompatibleECodesA = function(cars){
+	return new Promise(function(resolve, reject){
+		Car
+			.aggregate()
+				// filter the cars...
+				.match(cars)
+				// collect all the ecode sets...
+				.group({
+					_id: null, 
+					ecodes: { $push: '$ecodes' }})
+				// intersect the ecode sets...
+				.project({ecodes: { $setIntersection: '$ecodes' }})
+				// get the result...
+				.exec()
+					.then(function(data){
+						resolve(data.ecodes)	
+					})
+					.then(null, function(err){
+						reject(err)
+					})
+	}
+}
+
+
+// XXX test...
+Car.getCompatibleECodesMR = function(cars){
+	var res = []
+
+	return new Promise(function(resolve, reject){
+		Car
+			.mapReduce({
+					query: cars,
+
+					map: function(){ 
+						emit(this.ecodes, 1) 
+					},
+					reduce: function(k, vals){ 
+						var res = vals.splice(0, 1)
+
+						for(var i=0; i < vals.length; i++){
+							if(res.length == 0){
+								return res
+							}
+							var cur = vals[i]
+							res = res.filter(function(e){
+								return cur.indexOf(e) >= 0
+							})
+						}
+
+						return res
+					},
+				},
+				function(err, data){
+					resolve(data)
+				})
+	})
+}
+
+
 // vim:set ts=4 sw=4 nowrap :
